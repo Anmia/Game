00af67c3409c1f9b165b8d61da7e69e3d30bed35
Anmia <nerdikus_vulgeris@hotmail.com>
2017-12-26 23:44:50 +0100

    16a6dd77cae4

---
+++ src/game/Melee_Glaive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author Magnus
+ */
+public class Melee_Glaive extends Melee {
+    private final static int itemID = 11032;
+    private final static String name = "Glaive";
+    
+    private final static boolean twoHanded = true;
+    private final static int damageDice = 10;
+    
+    private final static boolean reach = true;
+    private static final char damageType = 's';
+
+    public Melee_Glaive() {
+        super(itemID, name, twoHanded, damageDice, reach, damageType);
+    }
+}
--- src/game/Melee_Greataxe.java
+++ src/game/Melee_Greataxe.java
@@ -9,17 +9,17 @@ package game;
  *
  * @author Magnus
  */
-public class Melee_Flail extends Melee {
-    private final static int itemID = 11031;
-    private final static String name = "Flail";
+public class Melee_Greataxe extends Melee {
+    private final static int itemID = 11033;
+    private final static String name = "Handaxe";
     
-    private final static boolean twoHanded = false;
-    private final static int damageDice = 8;
+    private final static boolean twoHanded = true;
+    private final static int damageDice = 12;
     
     private final static boolean reach = false;
-    private static final char damageType = 'b';
+    private static final char damageType = 's';
 
-    public Melee_Flail() {
+    public Melee_Greataxe() {
         super(itemID, name, twoHanded, damageDice, reach, damageType);
     }
 }
