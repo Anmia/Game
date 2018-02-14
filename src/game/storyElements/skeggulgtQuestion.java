128fe6a46d96445b1c8b209c6aab09a8e972df4b
Anmia <nerdikus_vulgeris@hotmail.com>
2018-02-14 14:05:05 +0100

    l-h fluff

--- src/game/storyElements/skeggulgtQuestion.java
+++ src/game/storyElements/skeggulgtQuestion.java
@@ -10,11 +10,51 @@ package game.storyElements;
  * @author Magnus
  */
 public class skeggulgtQuestion {
+    private int step = 0;
+    private String toPrint;
+    
+    private int[] optValue = new int[10];
+    private String[] optName = new String[10];
+    
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    
+    
     public skeggulgtQuestion() {
-	
+	optValue[9] = 9;
+        
+        fluffFunc();
     }
     
-    public void fluffText() {
-	
+    private void fluffFunc() {
+	boolean proceed = true;
+        
+        while (proceed) {
+            fluffText();
+            
+            for (int i = 0; i < optValue.length; i++) {
+                if (optName[i] != null) {
+                    System.out.println("< " + i + " > " + optName[i]);
+                }
+            }
+            
+            System.out.println("Please choose an option: ");
+            int choice = sc.nextInt();
+            
+            if (optValue[choice] == -1) {
+                System.out.println("Please choose a valid option");
+            } else {
+                step = optValue[choice];
+            }
+        }
+    }
+    
+    private void fluffText() {
+        switch (step) {
+            case 0: 
+                toPrint = "\"So my friend, have you ever been to Krohnhagen before?\"";
+                
+                
+                break;
+        }
     }
 }
